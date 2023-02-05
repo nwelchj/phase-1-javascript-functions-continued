1499ef9a187486c55154c074f6cb5f3f597ec526
Nolan <bugygamer.mc@gmail.com>
2023-02-04 22:54:54 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+
+function saturdayFun( Act = "roller-skate",){
+    return(`This Saturday, I want to ${Act}!`);
+
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`);
+
+}
+mondayWork()
+
+
+
+function wrapAdjective (greeting = "*", name = 'You are') {
+    const innerFunction = function (msg = "a dedicated programmer!") {
+      return (`${name} ${ greeting}${msg}${greeting}!`);
+
+
+    };
+    return innerFunction;
+  }
+  ;
